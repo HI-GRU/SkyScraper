f020564af878af43cd2f5de89e4bf8c6a97cc371
parseley <gruns0989@naver.com>
2024-11-16 11:52:33 +0900

    [feat] Add building unit class

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,5 +4,11 @@ public class Building
     public int SizeY { get; private set; } // 높이
     public int SizeZ { get; private set; }
 
-    
+    public Building(int SizeX, int SizeY, int SizeZ)
+    {
+        this.SizeX = SizeX;
+        this.SizeY = SizeY;
+        this.SizeZ = SizeZ;
+    }
+
 }
