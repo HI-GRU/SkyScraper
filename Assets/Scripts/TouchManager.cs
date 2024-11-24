b2f53cee1bc6d7e95eb735a056faef14b50dd972
parseley <gruns0989@naver.com>
2024-11-24 21:52:30 +0900

    [feat] Add building drag function

--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -2,5 +2,69 @@ using UnityEngine;
 
 public class TouchManager : MonoBehaviour
 {
+    [SerializeField] private LayerMask buildingLayer;
+    [SerializeField] private LayerMask tileLayer;
 
+    private readonly Plane XZPlane = new Plane(Vector3.up, 0);
+    private Camera mainCamera;
+    private GameObject selectedBuilding;
+    private Vector3 dragOffset;
+    private bool isDragging => selectedBuilding != null;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            HandleDragging();
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleSelecting();
+        }
+    }
+
+    private void HandleDragging()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            float distance;
+
+            if (XZPlane.Raycast(ray, out distance))
+            {
+                Vector3 hitPoint = ray.GetPoint(distance);
+                Vector3 newPosition = new Vector3(
+                    hitPoint.x - dragOffset.x,
+                    selectedBuilding.transform.position.y,
+                    hitPoint.z - dragOffset.z
+                );
+
+                selectedBuilding.transform.position = newPosition;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            selectedBuilding = null;
+            Debug.Log("Drag Off !!!");
+        }
+    }
+
+    private void HandleSelecting()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildingLayer))
+        {
+            selectedBuilding = hit.transform.gameObject;
+            dragOffset = hit.point - selectedBuilding.transform.position;
+        }
+    }
 }
