177046ee0178b9bd7aa2c300fb3fc816d82327f1
parseley <gruns0989@naver.com>
2024-12-01 10:24:49 +0900

    [feat] Add main scene

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject stagePanel;
+    AsyncOperation asyncLoad;
+
+    public async void LoadMainScene()
+    {
+        asyncLoad = SceneManager.LoadSceneAsync("MainScene");
+
+        while (!asyncLoad.isDone)
+        {
+            await System.Threading.Tasks.Task.Yield();
+        }
+    }
+
+    public async void LoadGameScene()
+    {
+        asyncLoad = SceneManager.LoadSceneAsync("GameScene");
+
+        while (!asyncLoad.isDone)
+        {
+            await System.Threading.Tasks.Task.Yield();
+        }
+    }
+
+    public void ShowStagePanel()
+    {
+        mainPanel.SetActive(false);
+        stagePanel.SetActive(true);
+    }
+
+    public void ShowMainPanel()
+    {
+        mainPanel.SetActive(true);
+        stagePanel.SetActive(false);
+    }
+}
