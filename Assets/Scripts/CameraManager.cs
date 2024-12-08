8498157d82967e0287d1d0b1f009ec6347a26fd2
HI-GRU <gruns0989@naver.com>
2024-12-08 17:59:49 +0900

    [feat] 카메라 회전 기능 구현

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 [DefaultExecutionOrder(-1)]
 public class CameraManager : MonoBehaviour
@@ -8,11 +11,17 @@ public class CameraManager : MonoBehaviour
     public Camera mainCamera { get; private set; }
     private Stage currentStage;
 
-    private Vector3 center;
-    private Vector3 cameraPosition => getCameraPos();
-    private const float angleX = 30F;
-    public float angleY = -45F;
+    private Vector3 center; // 타일 중심
+    private Vector3 cameraPosition => getCameraPos(); // 타일 중심을 기준으로 반지름만큼 떨어진 위치
+    private float angleX = 30F; // YZ 평면으로부터 회전한 값
+    private float angleY = -45F; // 카메라 회전 시 변하는 값
     private float radius => Math.Max(currentStage.size.x, Math.Max(currentStage.size.y, currentStage.size.z)) * 10F;
+
+    // 카메라 회전
+    private bool isDragging = false;
+    private float dragSensitivity = 0.4F;
+    private Vector2 dragStartPosition;
+    private const float ANGLE_SMOOTHING = 10F;
 
     private void Awake()
     {
@@ -23,9 +32,40 @@ public class CameraManager : MonoBehaviour
         mainCamera.orthographicSize = radius / 5F;
     }
 
-    private void Update()
+    private void Start()
     {
         SetMainView();
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            RotateCamera();
+            return;
+        }
+        if (Input.GetMouseButtonDown(0)) CheckRotate();
+    }
+
+    private void RotateCamera()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // 현재 마우스 위치와 드래그 시작 위치의 차이 계산
+            float dragDeltaX = Input.mousePosition.x - dragStartPosition.x;
+
+            // angleY 업데이트
+            float targetAngleY = angleY - dragDeltaX * dragSensitivity;
+
+            // angleY를 0 ~ 360 범위로 조정
+            angleY = (targetAngleY % 360 + 360) % 360;
+            SetMainView();
+            dragStartPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
     }
 
     private void SetMainView()
@@ -42,5 +82,33 @@ public class CameraManager : MonoBehaviour
         res.z = radius * Mathf.Cos(angleX * Mathf.Deg2Rad) * Mathf.Sin(angleY * Mathf.Deg2Rad);
 
         return res;
+    }
+
+    private void CheckRotate()
+    {
+        bool CanRotate = true;
+
+        // 3D 오브젝트 클릭 여부
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            CanRotate = !hit.collider.gameObject.activeInHierarchy;
+        }
+
+        // GamePanel 터치 여부 확인
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+            pointerData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            CanRotate &= "GamePanel".Equals(results[0].gameObject.tag);
+        }
+
+        isDragging = CanRotate;
+        if (isDragging) dragStartPosition = Input.mousePosition;
     }
 }
